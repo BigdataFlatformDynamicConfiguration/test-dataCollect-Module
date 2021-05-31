c4584525f2ce831998a2056b580165e0b11f489b
LeeHanSeong <jkhk7788@gmail.com>
2021-05-31 14:30:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const python = require('./modules/pythonM');
 const config = require('./config');
-const {head,data} = require('./modules/dbfParser').readFile(config.sourceInfo.path);
+//const {head,data} = require('./modules/dbfParser').readFile(config.sourceInfo.path);
+
+// python.getTest('/delete-table?table_name=Daegue').then((res)=>{
+//     console.log(res);
+// });
 
 // python.postTest('/create-table',{
 //      'table_name' : 'Daegue',
@@ -16,28 +20,38 @@ const {head,data} = require('./modules/dbfParser').readFile(config.sourceInfo.pa
 //     console.log(res);
 // });
 
-const keys = Object.keys(data[0]).slice(2);
-let j=0;
-let items = [];
-for(let i=0; i< data.length; i++){
-    let item = {};
-    item['ID:UFID'] = data[i]['UFID'];
-    item['ID:BLD_NM'] = data[i]['BLD_NM'];
-    item['ID:DONG_NM'] = data[i]['DONG_NM'];
-    for(let j=3; j<keys.length; j++){
-        item['DATA:'+keys[j]] = data[i][keys[j]];
-    }
-    j += 1;
-    items.push(item);
-    if (j == 10){
-        python.postTest('/put-rows',{
-             'table_name' : 'Daegue',
-             'data' : items,
-        }).then((res)=>{
-             console.log(i);
-        });
-        //console.log(items);
-        j = 0;
-        items = [];
-    }
-}
+// const keys = Object.keys(data[0]).slice(2);
+// let idx=0;
+// let tester = 1;
+// const timer = setInterval(()=>{
+//     let items = [];
+//     for(let i=0; i<100;i++){
+//         let item = {};
+//         item['ID:UFID'] = data[idx]['UFID'];
+//         item['ID:BLD_NM'] = data[idx]['BLD_NM'];
+//         item['ID:DONG_NM'] = data[idx]['DONG_NM'];
+//         for(let j=3; j<keys.length; j++){
+//             item['DATA:'+keys[j]] = data[idx][keys[j]].toString();
+//         }
+//         items.push(item);
+//         idx += 1;
+//         if (idx == data.length){
+//             clearInterval(timer);
+//             break;
+//         }
+//     }
+//     python.postTest('/put-rows',{
+//         'table_name' : 'Daegue',
+//         'data' : items,
+//     }).then((res)=>{
+//         console.log(tester);
+//         tester += 1;
+//     });
+// },1000);
+
+python.postTest('/scan',{
+     'table' : 'Daegue',
+     'filter' : "SingleColumnValueFilter ('ID','BLD_NM',=,'경북대학교')",
+}).then((res)=>{
+     console.log(res);
+});
