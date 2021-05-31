3fd009e9ae676241d2bbf2fba281a6fdd9811ffe
LeeHanSeong <jkhk7788@gmail.com>
2021-06-01 02:45:13 +0900

    u

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const python = require('./modules/pythonM');
 const config = require('./config');
 const parser = require("xlsx");
 
-let excel = parser.readFile(config.sourceInfo.path);
-let data = excel.Sheets["F_FAC_BUILDING_27_202105"];
+// let excel = parser.readFile(config.sourceInfo.path);
+// let data = excel.Sheets["F_FAC_BUILDING_27_202105"];
 
 // console.log(data['B6']);
 
@@ -29,47 +29,47 @@ let data = excel.Sheets["F_FAC_BUILDING_27_202105"];
 //     console.log(res);
 // });
 
-function emptyChecker(item, value, target){
-    if (value != undefined)
-        item[target] = value['v'].toString();
-}
+// function emptyChecker(item, value, target){
+//     if (value != undefined)
+//         item[target] = value['v'].toString();
+// }
 
-const cols = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-const keys = cols.map(i=>{
-    return data[i+'1']['v'];
-});
-let idx=2;
-let tester = 1;
-const timer = setInterval(()=>{
-    let items = [];
-    for(let i=0; i<100;i++){
-        let item = {};
-        emptyChecker(item,data['A'+idx],'ID:UFID');
-        emptyChecker(item,data['B'+idx],'ID:BLD_NM');
-        emptyChecker(item,data['C'+idx],'ID:DONG_NM');
-        for(let j=3; j<keys.length; j++){
-            emptyChecker(item,data[cols[j]+idx],'DATA:'+keys[j]);
-        }
-        if (Object.keys(item).length == 0){
-            console.log('end');
-            clearInterval(timer);
-            break;
-        }
-        console.log(idx);
-        items.push(item);
-        idx += 1;
-    }
-    python.postTest('/put-rows',{
-        'table_name' : 'Daegue',
-        'data' : items,
-    }).then((res)=>{
-        console.log(tester);
-        tester += 1;
-    });
-},1000);
+// const cols = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+// const keys = cols.map(i=>{
+//     return data[i+'1']['v'];
+// });
+// let idx=2;
+// let tester = 1;
+// const timer = setInterval(()=>{
+//     let items = [];
+//     for(let i=0; i<100;i++){
+//         let item = {};
+//         emptyChecker(item,data['A'+idx],'ID:UFID');
+//         emptyChecker(item,data['B'+idx],'ID:BLD_NM');
+//         emptyChecker(item,data['C'+idx],'ID:DONG_NM');
+//         for(let j=3; j<keys.length; j++){
+//             emptyChecker(item,data[cols[j]+idx],'DATA:'+keys[j]);
+//         }
+//         if (Object.keys(item).length == 0){
+//             console.log('end');
+//             clearInterval(timer);
+//             break;
+//         }
+//         console.log(idx);
+//         items.push(item);
+//         idx += 1;
+//     }
+//     python.postTest('/put-rows',{
+//         'table_name' : 'Daegue',
+//         'data' : items,
+//     }).then((res)=>{
+//         console.log(tester);
+//         tester += 1;
+//     });
+// },1000);
 python.postTest('/scan',{
      'table' : 'Daegue',
-     'filter' : "SingleColumnValueFilter ('ID','UFID',=,'2002164791842653460600000000')",
+     'filter' : "SingleColumnValueFilter('ID','UFID',=,'binary:2003162242572638627600000000')",
 }).then((res)=>{
      console.log(res);
 });
